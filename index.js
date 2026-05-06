b1cdd209bd06db488620a63f41f4c06b0f48e9b3
unknown <amirahmed7208@gmail.com>
2026-05-06 21:03:51 +0600

    fifth commit

--- index.js
+++ index.js
@@ -74,7 +74,11 @@ async function run(){
     })
 
     //post review
-    
+    app.post('/reviews', async (req, res) => {
+        const review = req.body;
+        const result = await reviewsCollection.insertOne(review);
+        res.send(result);
+    })
 
     // delete a food
    
