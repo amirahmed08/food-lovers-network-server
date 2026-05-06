4d82959d1e5085f5f4f65b2d6317dfce9249a9e2
unknown <amirahmed7208@gmail.com>
2026-05-06 21:03:00 +0600

    fourth commit

--- index.js
+++ index.js
@@ -66,7 +66,12 @@ async function run(){
         res.send(result);
     })
 
-    
+    app.get('/foods/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId (id) };
+        const result = await foodsCollection.findOne(query);
+        res.send(result); 
+    })
 
     //post review
     
