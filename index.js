0b400686b6c503bd19754d7c8e46e424e81a7b07
unknown <amirahmed7208@gmail.com>
2026-05-06 21:05:04 +0600

    sixth commit

--- index.js
+++ index.js
@@ -81,7 +81,12 @@ async function run(){
     })
 
     // delete a food
-   
+    app.delete('/foods/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId (id) };
+        const result = await foodsCollection.deleteOne(query);
+        res.send(result); 
+    })
 
 
     await client.db("admin").command({ ping : 1});
